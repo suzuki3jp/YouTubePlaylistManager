1f5985164f2cffa09b5690577ec0f293bf57f32c
suzuki3jp <suzuki3.info@gmail.com>
2024-10-25 06:04:14 +0900

    chore: 不要なコードを削除

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,9 +1,6 @@
-import { hoge } from "@playlistmanager/youtube-adapter";
-
 import { GoogleLoginButton, SignoutButton } from "@/components";
 
 export default async function Home() {
-	hoge();
 	return (
 		<>
 			<GoogleLoginButton />
