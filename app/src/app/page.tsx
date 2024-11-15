6d2cec820f2794bc6ce56ffc5e23c38e024a93eb
suzuki3jp <suzuki3.info@gmail.com>
2024-11-16 07:07:20 +0900

    chore: html lang 属性を適切な値に変更する

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,10 +1,18 @@
 "use client";
 import { PlaylistManager, SnackbarProvider } from "@/components";
+import { useT } from "@/hooks";
 import { darkTheme } from "@/themes";
 import { ThemeProvider } from "@mui/material";
 import { SessionProvider } from "next-auth/react";
+import { useEffect } from "react";
 
 export default function Home() {
+	// TODO: 現状 layout.tsx でクエリを取得する方法がないっぽかったから回避策でこれ
+	const { lang } = useT();
+	useEffect(() => {
+		document.documentElement.setAttribute("lang", lang);
+	}, [lang]);
+
 	return (
 		<ThemeProvider theme={darkTheme}>
 			<SessionProvider>
