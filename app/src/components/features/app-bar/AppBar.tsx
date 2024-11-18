599df3d72f809d8dd7a5291d14a4dec65fb68631
suzuki3jp <suzuki3.info@gmail.com>
2024-11-18 17:36:55 +0900

    chore: `AppBar` を横幅の小さい端末でも表示崩れがないように変更

--- app/src/components/features/app-bar/AppBar.tsx
+++ app/src/components/features/app-bar/AppBar.tsx
@@ -17,12 +17,36 @@ export const AppBar = async () => {
 			position="static"
 			sx={{
 				bgcolor: "grey.900",
+				paddingRight: { xs: 0, md: "20%" },
+				paddingLeft: { xs: 0, md: "20%" },
 			}}
 		>
-			<Toolbar>
-				<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+			<Toolbar
+				sx={{
+					flexDirection: { xs: "column", sm: "row" },
+					gap: { xs: 2, sm: 0 },
+					padding: {
+						xs: 2,
+						sm: "0 24px",
+					},
+					minHeight: {
+						xs: "auto",
+						sm: "64px",
+					},
+				}}
+			>
+				<Typography
+					variant="h6"
+					component="div"
+					sx={{
+						flexGrow: { sm: 1 },
+						textAlign: { xs: "center", sm: "left" },
+						width: { xs: "100%", sm: "auto" },
+					}}
+				>
 					PlaylistManager
 				</Typography>
+
 				<Grid container spacing={2}>
 					<LanguageSwitcher />
 					{session ? <GoogleSignOutButton /> : <GoogleSignInButton />}
