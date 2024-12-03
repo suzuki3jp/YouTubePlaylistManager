fbd9b8f8347ea0a4a841d161be4f6c83bd583311
suzuki3jp <suzuki3.info@gmail.com>
2024-12-03 15:36:04 +0900

    refactor: HomeUI を削除

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,10 +1,20 @@
-import { HomeUI, Layout } from "@/components";
+import {
+	ClientProvidersProvider,
+	Layout,
+	PlaylistItemBrowser,
+	PlaylistManager,
+} from "@/components";
 import type { PageProps } from "@/types";
 
 export default async function Home({ searchParams }: PageProps) {
+	const id = (await searchParams).id;
+	const ids = typeof id === "string" ? id.split(",") : id;
+
 	return (
 		<Layout searchParams={searchParams}>
-			<HomeUI />
+			<ClientProvidersProvider>
+				{ids ? <PlaylistItemBrowser ids={ids} /> : <PlaylistManager />}
+			</ClientProvidersProvider>
 		</Layout>
 	);
 }
--- app/src/components/features/index.ts
+++ app/src/components/features/index.ts
@@ -1,6 +1,5 @@
 export * from "./app-bar";
 export * from "./full-playlist-card";
-export * from "./home-ui";
 export * from "./layout";
 export * from "./playlist-card";
 export * from "./playlist-item-browser";
