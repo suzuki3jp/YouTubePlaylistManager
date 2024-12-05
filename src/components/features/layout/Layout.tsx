5cca72168a11f585f02a5cedebdb32cb1ae0b2fb
suzuki3jp <suzuki3.info@gmail.com>
2024-12-06 02:57:37 +0900

    chore: Add Google Analytics support

--- src/components/features/layout/Layout.tsx
+++ src/components/features/layout/Layout.tsx
@@ -2,6 +2,8 @@ import { AppBar, ServerProvidersProvider } from "@/components";
 import { Footer } from "@/components/features/footer";
 import { QUERY_NAME, getSafeLang } from "@/locales/settings";
 import type { PageProps } from "@/types";
+import { getEnv } from "@/utils";
+import Script from "next/script";
 import type React from "react";
 import type { PropsWithChildren } from "react";
 
@@ -11,9 +13,26 @@ export const Layout: React.FC<LayoutProps> = async ({
 }) => {
 	const lang = (await searchParams)[QUERY_NAME];
 	const resolvedLang = getSafeLang(lang);
+	const gaIdResult = getEnv(["GOOGLE_ANALYTICS_ID"]);
+	const gaId = gaIdResult.isFailure() ? "" : gaIdResult.data[0];
 
 	return (
 		<html lang={resolvedLang} style={{ height: "100%" }}>
+			<head>
+				<Script
+					src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
+					strategy="afterInteractive"
+					async
+				/>
+				<Script id="google-analytics" strategy="afterInteractive">
+					{`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gaId}');
+          `}
+				</Script>
+			</head>
 			<body
 				style={{
 					margin: "0 0 0 0",
