83d08883a9a51b55472653d64f14ed4a51c9de83
suzuki3jp <suzuki3.info@gmail.com>
2024-12-06 02:09:46 +0900

    fix: Fix type error when `next build` [See](https://stackoverflow.com/questions/76388994/next-js-13-4-and-nextauth-type-error-authoptions-is-not-assignable-to-type-n)

---
+++ src/app/api/auth/[...nextauth]/nextAuthOptions.ts
@@ -0,0 +1,40 @@
+import { getEnv } from "@/utils";
+import type { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+const r = getEnv(["GOOGLE_CLIENT_ID", "GOOGLE_CLIENT_SECRET"]);
+
+if (r.isFailure()) throw r.data;
+const [clientId, clientSecret] = r.data;
+
+export const OPTIONS: NextAuthOptions = {
+	session: {
+		strategy: "jwt",
+	},
+	providers: [
+		GoogleProvider({
+			clientId,
+			clientSecret,
+			authorization: {
+				params: {
+					scope: [
+						"https://www.googleapis.com/auth/youtube",
+						"https://www.googleapis.com/auth/userinfo.profile",
+						"https://www.googleapis.com/auth/userinfo.email",
+					].join(" "),
+				},
+			},
+		}),
+	],
+	callbacks: {
+		async jwt({ token, account }) {
+			if (account) {
+				token.accessToken = account.access_token;
+			}
+			return token;
+		},
+		async session({ session, token }) {
+			session.accessToken = token.accessToken;
+			return session;
+		},
+	},
+};
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,7 @@
-import { getEnv } from "@/utils";
-import type { NextAuthOptions } from "next-auth";
 import type { DefaultSession } from "next-auth";
 import NextAuth from "next-auth/next";
-import GoogleProvider from "next-auth/providers/google";
+
+import { OPTIONS } from "./nextAuthOptions";
 
 declare module "next-auth" {
 	interface Session extends DefaultSession {
@@ -18,44 +17,6 @@ declare module "next-auth/jwt" {
 		accessToken?: string;
 	}
 }
-
-const r = getEnv(["GOOGLE_CLIENT_ID", "GOOGLE_CLIENT_SECRET"]);
-
-if (r.isFailure()) throw r.data;
-const [clientId, clientSecret] = r.data;
-
-export const OPTIONS: NextAuthOptions = {
-	session: {
-		strategy: "jwt",
-	},
-	providers: [
-		GoogleProvider({
-			clientId,
-			clientSecret,
-			authorization: {
-				params: {
-					scope: [
-						"https://www.googleapis.com/auth/youtube",
-						"https://www.googleapis.com/auth/userinfo.profile",
-						"https://www.googleapis.com/auth/userinfo.email",
-					].join(" "),
-				},
-			},
-		}),
-	],
-	callbacks: {
-		async jwt({ token, account }) {
-			if (account) {
-				token.accessToken = account.access_token;
-			}
-			return token;
-		},
-		async session({ session, token }) {
-			session.accessToken = token.accessToken;
-			return session;
-		},
-	},
-};
 
 const handler = NextAuth(OPTIONS);
 export { handler as GET, handler as POST };
