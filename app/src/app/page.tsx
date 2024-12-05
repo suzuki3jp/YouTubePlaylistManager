686f5bfa17352e4f2b0757a1eea6696b8ececb01
suzuki3jp <suzuki3.info@gmail.com>
2024-12-05 18:18:54 +0900

    chore: `PlaylistManager`, `PlaylistItemBrowser` に `description` を追加

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -6,6 +6,7 @@ import {
 	PlaylistManager,
 	SectionWithTitle,
 } from "@/components";
+import { useServerT } from "@/hooks";
 import type { PageProps } from "@/types";
 import { Box, Typography } from "@mui/material";
 import { getServerSession } from "next-auth";
@@ -14,6 +15,7 @@ export default async function Home({ searchParams }: PageProps) {
 	const session = await getServerSession();
 	const id = (await searchParams).id;
 	const ids = typeof id === "string" ? id.split(",") : id;
+	const { t } = await useServerT(searchParams);
 
 	return (
 		<Layout searchParams={searchParams}>
@@ -25,7 +27,16 @@ export default async function Home({ searchParams }: PageProps) {
 				<ClientProvidersProvider>
 					{session ? (
 						<SectionWithTitle
-							title={ids ? "Playlist item browser" : "PlaylistManager"}
+							title={
+								ids
+									? t("playlist-item-browser.title")
+									: t("playlist-manager.title")
+							}
+							subtitle={
+								ids
+									? t("playlist-item-browser.description")
+									: t("playlist-manager.description")
+							}
 							sx={{
 								my: "1%",
 							}}
@@ -36,6 +47,17 @@ export default async function Home({ searchParams }: PageProps) {
 						</SectionWithTitle>
 					) : null}
 				</ClientProvidersProvider>
+				<SectionWithTitle title="About">
+					<Box ml={"10%"}>
+						<Typography fontWeight={"bold"} fontSize={20} gutterBottom>
+							What is YouTubePlaylistManager
+						</Typography>
+						<Typography gutterBottom>
+							YouTubePlaylistManager is a web application that allows you to
+							manage your YouTube playlists and videos in a more efficient way.
+						</Typography>
+					</Box>
+				</SectionWithTitle>
 			</CenteredLayout>
 		</Layout>
 	);
